f0f1709da858e27bb4e69cbaf4254fcbfe7ff7c9
Mike Shevchuk <mikhail.shevchuk@gmail.com>
2021-09-06 13:04:25 +0300

    clean up *.js files

--- data/devices.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.devices = void 0;
-exports.devices = [
-    [0, 0],
-    [100, 100],
-    [15, 10],
-    [18, 18]
-];
--- data/link-stations.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.linkStations = void 0;
-exports.linkStations = [
-    [0, 0, 10],
-    [20, 20, 5],
-    [10, 0, 12]
-];
